c3f00c4471ae9f32064030ae66ddfac99229c68a
rorylikessharks <rorys@lindenschool.ca>
2025-05-02 10:31:51 -0400

    fish

---
+++ btn.js
@@ -0,0 +1,4 @@
+var fish2 = document.getElementById("fish2")
+fish2.addEventListener("click",function(){
+  alert("goodbye!!")
+})
---
+++ button.js
@@ -0,0 +1,6 @@
+// the id for the button and alert will go here
+
+var fsh1 = document.getElementById('fish1')
+fsh1.addEventListener('click',function(){
+    alert('hellooooooo!')
+})
