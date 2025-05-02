3a96087804d6f9508e5b9fa767f76e367b5e98ef
Hannah-Code-11 <hannahk@lindenschool.ca>
2025-05-02 09:55:15 -0400

    Update button.js

--- button.js
+++ button.js
@@ -1,6 +1,7 @@
 // the id for the button and alert will go here
 
 var fsh1 = document.getElementById('fish1')
-if(fsh1 == 'click'){
+fsh1 = false;
+if (fsh1 == true) {
     alert('hellooooooo!')
 }
