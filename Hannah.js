aaee9ef2491018ba99e790274718e5a20e16bc07
Hannah-Code-11 <hannahk@lindenschool.ca>
2025-05-07 15:30:56 -0400

    hypatia

--- Hannah.js
+++ Hannah.js
@@ -1,11 +1,11 @@
-//gurl mathers - Hypatia
+//gurl math-ers - Hypatia
 
 var hyp = document.getElementById('h-dropdown')
 var instances;
 var open;
+var facts = document.getElementsByClassName('collapsible')
 
 hyp.addEventListener('click', function () {
     alert('yay!')
+    facts.open
 })
-
-
