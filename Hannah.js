a3962faafe4df1110603a46f402eef2b18688912
Hannah-Code-11 <hannahk@lindenschool.ca>
2025-05-08 09:56:51 -0400

    if

--- Hannah.js
+++ Hannah.js
@@ -1,11 +1,22 @@
 //gurl math-ers - Hypatia
 
 var hyp = document.getElementById('h-dropdown')
-var instances;
-var open;
-var facts = document.getElementsByClassName('collapsible')
+var i;
+var facts = document.getElementsById('h-dropdown')
 
 hyp.addEventListener('click', function () {
     alert('yay!')
     facts.open
 })
+
+for (i = 0; i < facts.length; i++) {
+    facts[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
